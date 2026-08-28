2cd82cd552a34f10a064fa7c6806fd4a45d5df5d
AgneDJ <fizikee@gmail.com>
2026-08-28 09:36:47 +0300

    Update attendance web app endpoint

--- checkin/config.js
+++ checkin/config.js
@@ -1,4 +1,4 @@
 /* Paste the /exec URL created by checkin/google-apps-script/README.md here. */
 window.CHECKIN_CONFIG = {
-  endpoint: 'https://script.google.com/macros/s/AKfycbzFmAOlssOXPsrFdbu28qZL5Ets-bLmM9aIjK7m_OjqTpVijv19M2k26TMJdBzFPHGn/exec'
+  endpoint: 'https://script.google.com/macros/s/AKfycbzaMvSpBKw_S8Syj5qFNAkiqb9zbfTsXHiH4nPbe151yk40Arlf2Z6tc1HItirF4Hj1/exec'
 };
